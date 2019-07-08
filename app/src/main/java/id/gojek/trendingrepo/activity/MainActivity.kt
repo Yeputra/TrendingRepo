996944e950f4ae6f2519739de1812d7699545f2b
Yuda Eka Putra <yuda702@gmail.com>
2019-07-09 01:48:28 +0700

    feat: Added swipeRefresh Layout.

--- app/src/main/java/id/gojek/trendingrepo/activity/MainActivity.kt
+++ app/src/main/java/id/gojek/trendingrepo/activity/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.faltenreich.skeletonlayout.Skeleton
 import com.faltenreich.skeletonlayout.applySkeleton
 import com.google.gson.Gson
 import id.gojek.trendingrepo.R
 import id.gojek.trendingrepo.api.ApiRepository
 import id.gojek.trendingrepo.model.Repo
+import org.jetbrains.anko.support.v4.onRefresh
 
 class MainActivity : AppCompatActivity(), MainView {
 
@@ -19,12 +21,14 @@ class MainActivity : AppCompatActivity(), MainView {
     private lateinit var adapter: MainAdapter
     private lateinit var skeleton: Skeleton
     private lateinit var rvRepo: RecyclerView
+    private lateinit var slParent: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         rvRepo = findViewById(R.id.rv_trending_repo)
+        slParent = findViewById(R.id.sl_parent)
         adapter = MainAdapter(this, MutableRepoCollection)
         rvRepo.adapter = adapter
         rvRepo.layoutManager = LinearLayoutManager(this)
@@ -38,6 +42,10 @@ class MainActivity : AppCompatActivity(), MainView {
         presenter = MainPresenter(this, request, gson)
 
         presenter.getRepoList()
+
+        slParent.onRefresh {
+            presenter.getRepoList()
+        }
     }
 
     override fun showSkeleton() {
@@ -49,6 +57,7 @@ class MainActivity : AppCompatActivity(), MainView {
     }
 
     override fun showRepoList(repoCollection: List<Repo>) {
+        slParent.isRefreshing = false
         MutableRepoCollection.clear()
         MutableRepoCollection.addAll(repoCollection)
         adapter.notifyDataSetChanged()
